12e989ccb058491369a9fa5953a13e47f2f469d5
Yukiyo Torai <yuki@pedros-mbp.home>
2021-03-01 17:31:18 +0000

    Initial commit after setting up React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     {/* <!-- Item Start --> */}
+  Photosnap
+  New!
+  Featured
+  Senior Frontend Developer
+  1d ago
+  Full Time
+  USA only
+  {/* <!-- Role --> */}
+  Frontend
+  {/* <!-- Level --> */}
+  Senior
+  {/* <!-- Languages --> */}
+  HTML
+  CSS
+  JavaScript
+  {/* <!-- Item End --> */}
+
     </div>
   );
 }
